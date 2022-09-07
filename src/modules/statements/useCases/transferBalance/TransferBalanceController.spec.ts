9c92a74d0f8f5bce0e8fac777e0ca2c28cd9d302
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-09-07 18:08:56 -0300

    challange completed

--- src/modules/statements/useCases/transferBalance/TransferBalanceController.spec.ts
+++ src/modules/statements/useCases/transferBalance/TransferBalanceController.spec.ts
@@ -7,16 +7,15 @@ import { app } from "../../../../app";
 let connection: Connection;
 
 describe(" Transfer Balance Controller", () => {
+  const id2 = uuidV4();
 
   beforeAll( async () => {
     connection = await createConnection();
     await connection.runMigrations();
 
     const id = uuidV4();
-    const id2 = uuidV4();
 
     const password = await hash("admin", 8);
-    const passwordUser = await hash("user", 8);
 
     await connection.query(
       `INSERT INTO users (id, name, email, password, created_at, updated_at )
@@ -46,8 +45,43 @@ describe(" Transfer Balance Controller", () => {
 
     const { user, token } = responseToken.body;
 
+    await request(app).post("/api/v1/statements/deposit").send({
+      amount: 100,
+      description: "deposit quantity"
+    }).set({
+      Authorization:  `Bearer ${token}`,
+      Headers: {
+        user: user.id,
+      }
+    })
 
+    const result = await request(app).post(`/api/v1/statements/transfers/${id2}`).send({
+      amount: 100,
+      description: "transfer quantity"
+    }).set({
+      Authorization:  `Bearer ${token}`,
+      Headers: {
+        user: user.id,
+      }
+    })
 
+    const responseTokenUserTarget = await request(app).post('/api/v1/sessions').send({
+      email: "gabriel@hotmail.com",
+      password: "admin"
+    })
+
+    const balance = await request(app).get(`/api/v1/statements/balance`).send({
+      amount: 100,
+      description: "transfer quantity"
+    }).set({
+      Authorization:  `Bearer ${responseTokenUserTarget.body.token}`,
+      Headers: {
+        user: responseTokenUserTarget.body.id,
+      }
+    })
+
+    expect(result.body).toEqual({transfer: "Transfer successfully"})
+    expect(balance.body.statement[0]).toHaveProperty("sender_id")
 
   })
 })
